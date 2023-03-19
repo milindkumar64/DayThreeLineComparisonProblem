1e36b8149d96c85f2973bb6722a7368630f5c0f1
milindkumar64 <milindgupta@gmail.com>
2023-03-19 21:43:51 +0530

    Add: Welcome Message

---
+++ DisplayWelcomeMessage.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.ucone;
+
+
+
+public class DisplayWelcomeMessage {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Welcome To Line Comparison Program :");
+		
+		
+	}
+
+}
